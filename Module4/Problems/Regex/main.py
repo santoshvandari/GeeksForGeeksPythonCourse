3ad5a1b6043c582339427dfdfd2385964270fae5
Santosh Bhandari <santoshvandari1@gmail.com>
2025-01-07 19:23:33 +0545

    Module 4 Practice

--- Module4/Problems/Regex/main.py
+++ Module4/Problems/Regex/main.py
@@ -1,15 +1,35 @@
-import re
+
+import re ##import re module to use regex
+
+# } Driver Code Ends
+#User function Template for python3
+
+
+
 def numberMatcher(str):
-    pat=re.compile("[0-9]")
-    match=re.findall(pat,str) #ind all finds all the matched texts and returns a list
-    print(match)
+    pat=re.compile("\d+")
+    match=re.findall(pat,str)#ind all finds all the matched texts and returns a list
     if(match): 
         for i in match:
             print(i, end=" ")
     else:
         print(-1,end="")
 
-str = "asdasd123asmdasdk34234kfdsd34sdfk5"
-print()
-numberMatcher(str)
-print()
+
+#{ 
+ # Driver Code Starts.
+
+def main():
+    testcases = int(input()) #testcases
+    while(testcases > 0):
+        str = input()
+        numberMatcher(str)
+        print()
+        testcases -= 1
+        
+
+
+        print("~")
+if __name__=='__main__':
+    main()
+# } Driver Code Ends
