49b9d70da21bf2ae65bb54198a43d06113ad659f
Santosh Bhandari <santoshvandari1@gmail.com>
2025-01-09 18:18:39 +0545

    Module 6 Question Solving

---
+++ Module6/Problems/Dictionary in Python - II/main.py
@@ -0,0 +1,58 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+
+# Function to check if pair
+# with given sum exists
+def pair_sum(dict, N, arr, sum):
+
+    # Your code here
+    # Hint: You can use 'in' to find if any key is in dict
+    for i in range(N):
+        for j in range(i):
+            newsum=arr[i]+arr[j]
+            if newsum==sum:
+                return True
+            
+
+    return False
+
+
+#{ 
+ # Driver Code Starts.
+# Driver code
+def main():
+    
+    # testcase input
+    testcase = int(input())
+    
+    # looping through testcases
+    while(testcase > 0):
+        
+        N = int(input())
+        sum = int(input())
+        dict = {}
+        x = N
+        arr = [int(i) for i in (input().split())]
+        
+        for i in arr:
+            dict[i] = 0
+                
+        for i in arr:
+            dict[i] +=1
+    
+        if pair_sum(dict, N, arr, sum) is True:
+            print ("Yes")
+        else:
+            print ("No")
+        print("~")
+        testcase -= 1
+
+
+if __name__ == '__main__':
+    main()
+# } Driver Code Ends
